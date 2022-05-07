932e956bb8aa114f05494920bdb74392abaae98c
Eetu <eetu.seppanen99@gmail.com>
2022-05-07 14:16:47 +0300

    Routes are now added to a database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
+
 const app = express();
 const PORT = 8000;
+const db = process.argv[2]; //Read db address from command line arguments
+const dbName = "sendIt"
+
+const dbClient = new MongoClient(db);
 
 app.use(express.json());
 app.use(cors());
 
 app.post('/saveRoute', function (req, res, next) {
-   console.log(req.body); 
+    dbClient.connect ((err, client) => {
+        if (err) {
+            console.log(err);
+        }
+        const db = client.db(dbName);
+        db.collection("routes").insertOne({
+            name: req.body.name,
+            description: req.body.description,
+            feet: req.body.feet,
+            grade: req.body.grade,
+            holds: req.body.holds
+        });
+    });
    res.status(202).send();
 });
 
